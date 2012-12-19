dd9a190e5e4209f29926a30d74d1bc83be283803
Rufus Pollock <rufus.pollock@okfn.org>
2012-12-19 20:56:42 +0000

    [viewer][s]: enable map and graph views.

--- viewer.js
+++ viewer.js
@@ -27,6 +27,20 @@ function makeView(datasetInfo) {
        view: new recline.View.SlickGrid({
          model: dataset
        })
+     },
+     {
+       id: 'graph',
+       label: 'Graph',
+       view: new recline.View.Graph({
+         model: dataset
+       })
+     },
+     {
+       id: 'map',
+       label: 'Map',
+       view: new recline.View.Map({
+         model: dataset
+       })
      }
   ];
 
